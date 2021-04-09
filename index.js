b902ae959065fd5d8b4589c0c0354f3cf81406ee
Abdul Alim <abdul.alim@zohocorp.com>
2021-04-09 14:11:53 +0530

    Cleanup

--- index.js
+++ index.js
@@ -4,94 +4,72 @@ const git = require('simple-git/promise')(directoryPath);
 
 const utils = require('./utils');
 const STATE_CODES = require('./state-code');
-const outDir = 'news';
+const LOG = console.log;
 
-function getNewDate() {
-    let date = new Date();
-    var utcTime = date.getTime() + date.getTimezoneOffset() * 60 * 1000;
-    let indiaOffset = 19800000;
-    return new Date(utcTime + indiaOffset);
-}
 
-const getCurrentISTLogTime = () => {
-    let dt = getNewDate();
-    return `${(dt.getMonth() + 1).toString().padStart(2, '0')}/${dt
-        .getDate()
-        .toString()
-        .padStart(2, '0')}/${dt
-        .getFullYear()
-        .toString()
-        .padStart(4, '0')} ${dt
-        .getHours()
-        .toString()
-        .padStart(2, '0')}:${dt
-        .getMinutes()
-        .toString()
-        .padStart(2, '0')}`;
-};
 
 
 (async function() {
-
-    const fetchNews = async function() {
+    /**
+     *
+     * @param states
+     * @returns {Promise<void>}
+     */
+    const fetchNews = async function(states) {
         try {
 
-            // git pull any remote changes
-            let pull = await git.pull();
-            console.log(pull);
+            let promises = states.map(([stateCode, stateName]) => {
+                LOG('Fetching:', stateName);
+                return utils.getNews(stateName, { code: stateCode, name: stateName });
+            });
 
-            console.log('Fetch Initiated at: ', getCurrentISTLogTime());
+            await Promise.all(promises).catch(e => LOG(e));
 
-            // let promises = Object.entries(STATE_CODES).map(([stateCode, stateName]) => {
-            //     return utils.getNews(stateName, { code: stateCode, name: stateName });
-            // });
-            //
-            //
-            // let articles = await Promise.all(promises);
-            //
-            // articles.forEach(({ articles, params }) => {
-            //     const filename = `./${outDir}/${params.code}.json`;
-            //     utils.cache(articles, filename);
-            // });
-
-            // check diff and commit
-            let { files: diffFiles } = await git.diffSummary();
-
-            const newsDir = `${directoryPath}/${outDir}/*`;
-
-            let gitAdd = await git.add("./news/*");
-
-            if (diffFiles.length) {
-
-
-
-                console.log(gitAdd);
-
-                return
-
-                let commit = await git.commit(`aws - data updated: ${getCurrentISTLogTime()}`);
-                console.log(commit);
-                let push = await git.push('origin', 'master');
-                console.log('pushed', push);
-            } else {
-                console.log('No updates since last run');
-            }
-
-            console.log('Finished at: ', getCurrentISTLogTime());
-            console.log('\n**************************************\n');
         } catch (e) {
             console.error(e);
         }
     };
 
+    async function fetchNewsInChunk() {
+
+        // git pull any remote changes
+        let pull = await git.pull();
+        LOG(pull);
+
+        LOG('Fetch Initiated at: ', utils.getCurrentISTLogTime());
+
+        // FETCHING
+        let chunks = utils.chunkArray(Object.entries(STATE_CODES), 3);
+        for (let stateChunk of chunks) {
+            await fetchNews(stateChunk);
+        }
+
+        // GIT PUSH OPERATIONS
+        let { files: diffFiles } = await git.diffSummary();
+        await git.add('./news/*');
+
+        if (diffFiles.length) {
+            let commit = await git.commit(`aws - data updated: ${utils.getCurrentISTLogTime()}`);
+            LOG(commit);
+            let push = await git.push('origin', 'main');
+            LOG('pushed', push);
+        } else {
+            LOG('No updates since last run');
+        }
+
+        LOG('Finished at: ', utils.getCurrentISTLogTime());
+        LOG('\n**************************************\n');
+    }
+
+
     // / minutes
     let every = 20;
     // fetch it every 20 minutes
     setInterval(async function() {
-        await fetchNews();
+        await fetchNewsInChunk();
     }, every * 60 * 1000);
 
-    fetchNews();
+    fetchNewsInChunk();
 
 })();
 
--- utils.js
+++ utils.js
@@ -1,10 +1,9 @@
 const GoogleNewsRss = require('google-news-rss');
 const getMetaData = require('metadata-scraper');
 const fs = require('fs');
-
-
+const outDir = 'docs';
 const googleNews = new GoogleNewsRss();
-
+const META_TIMEOUT = 1; // minutes
 
 /**
  *
@@ -17,8 +16,22 @@ function getNews(place = 'India', params) {
         googleNews
             .search(`Covid-19 ${place}`)
             .then(resp => {
-                console.log("fetched", place);
+
+                resp.sort(function(a, b) {
+                    var keyB = new Date(a.pubDate).getTime(),
+                        keyA = new Date(b.pubDate).getTime();
+                    // Compare the 2 dates
+                    if (keyA < keyB) return -1;
+                    if (keyA > keyB) return 1;
+                    return 0;
+                });
+
+                // latest 50 articles
+                resp.length = Math.min(resp.length, 50);
+
+                console.log('Meta:', place);
                 Promise.all(resp.map(article => getMeta(article))).then(articles => {
+                    console.log('Meta Received:', place);
                     articles = articles.map(article => {
                         let extra = article.extra || article;
                         return {
@@ -36,14 +49,20 @@ function getNews(place = 'India', params) {
                             }
                         };
                     });
-                    resolve({
-                        articles,
-                        params
-                    });
+
+                    const filename = `./${outDir}/${params.code}.json`;
+                    cache(articles, filename);
+                    resolve(true);
                 });
+
+                // TIMEOUT
+                setTimeout(() => {
+                    resolve(false);
+                }, 1000 * 60 * META_TIMEOUT);
+
             }).catch(e => {
             console.error(e);
-            resolve(true);
+            resolve(false);
         });
     });
 }
@@ -101,7 +120,42 @@ function getMeta(article) {
     });
 }
 
+function getNewDate() {
+    let date = new Date();
+    var utcTime = date.getTime() + date.getTimezoneOffset() * 60 * 1000;
+    let indiaOffset = 19800000;
+    return new Date(utcTime + indiaOffset);
+}
+
+const getCurrentISTLogTime = () => {
+    let dt = getNewDate();
+    return `${(dt.getMonth() + 1).toString().padStart(2, '0')}/${dt
+        .getDate()
+        .toString()
+        .padStart(2, '0')}/${dt
+        .getFullYear()
+        .toString()
+        .padStart(4, '0')} ${dt
+        .getHours()
+        .toString()
+        .padStart(2, '0')}:${dt
+        .getMinutes()
+        .toString()
+        .padStart(2, '0')}`;
+};
+
+function chunkArray(array, chunkSize = 10) {
+    var i, j, chunks = [];
+    for (i = 0, j = array.length; i < j; i += chunkSize) {
+        chunks.push(array.slice(i, i + chunkSize));
+    }
+    return chunks;
+}
+
 module.exports = {
+    getNewDate,
     getNews,
-    cache
+    cache,
+    getCurrentISTLogTime,
+    chunkArray
 };
